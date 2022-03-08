8236a8c5e617328aa42905fb766d4df5b1be3ca7
liupc <liucheng820272013@icloud.com>
2022-03-08 17:13:29 +0800

    push

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,18 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  configureWebpack:{
+    resolve:{
+     alias:{
+       'assets':'@/assets',
+       'common':'@/common',
+       'components':'@/components',
+       'network':'@/newwork',
+       'views':'@/views'
+
+     }
+    }
+  }
+}
